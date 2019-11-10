269439192f43b44b3e9056aed64283ba79cbe42d
Dan Shields <danwshields@gmail.com>
2019-11-09 21:02:45 -0700

    update per justin recommendations

--- src/pages/ethdenver.vue
+++ src/pages/ethdenver.vue
@@ -10,19 +10,17 @@
 
   </div> <!-- end hero -->
 
-  <div class="container-inner mx-auto pb-16">
+  <div class="container-inner mx-auto pb-10">
     <p class="text-xl">
-      <br> <strong><em class="text-blue-dark">Apro</em><em class="text-blue-light">flow</em></strong> is a fully-integrated educational program and network focused on emerging trends, curriculum enhancement, industry partnerships, and authentic student experiences. We offer ongoing curriculum integration opportunities and programming throughout the year as part of our flow with multiple university partner institutions.
-      <br><br>
-      As an ETHDenver partner, <strong><em class="text-blue-dark">Apro</em><em class="text-blue-light">flow</em></strong> is here to serve you and <strong><em>radically enhance the value</em></strong> you can expect from the premiere hackathon in the Ethereum ecosystem as part of our flow.  As a sponsor, you want the <strong>spotlight to be on you - <em>we can help you stand out among the crowd!</em></strong>
+      <strong><em class="text-blue-dark">Apro</em><em class="text-blue-light">flow</em></strong> and ETHDenver have partnered to offer ETHDenver sponsors with <b>local support</b> in coordinating events before, durring, and afer #BUIDL Week and
+      ETHDenver. We coordinator of year-round Colorado meet-ups and have strong connections with local universities, making it vastly easier for incoming sponsors to reach local students and blockchain enthusiasts.
       <br><br>
       <strong><em>We unbox the potential <u>around</u> ETHDenver and get you connected directly to attendees, so they go straight to using your products and services at ETHDenver and beyond.</em></strong>
     </p>
-
   </div>
 
   <div class="overflow-x-hidden">
-    <div class="services container-inner mx-auto text-xl border-t border-gray-500 border-b py-8 mb-8 relative">
+    <div class="services container-inner mx-auto text-xl border-t border-gray-500 py-8 mb-8 relative">
       <!-- top triangles -->
       <div class="absolute right-0" style="top: 10px; transform: translate(100%) rotate(180deg)">
         <svg width="140px" height="140px">
@@ -34,27 +32,40 @@
           <use xlink:href="#dots-triangle" /></svg>
       </div>
 
-      <h2 class="font-bold text-3xl text-center text-blue-light italic mb-2" id="services">Complete Event Services</h2>
-          <ul class="list-none text-center">
-            <li><u><em>You supply <strong>content</strong> --- we provide <strong>everything else</strong></em></u></li>
-            <li><em>Enhance Your Event with <strong>Customizations and Ala Carte Offerings</strong></em></li>
-            <li><em><strong> Explore</strong> Ongoing Partnership Opportunities</em></li>
+      <h2 class="font-bold text-4xl text-center text-blue-light italic mb-2" id="services">Complete Event Services</h2>
 
-          </ul>
-      <div class="flex justify-center object-center w-full mt-5 mb-6">
-        <a class="block bg-blue-light hover:bg-blue-700 tracking-wide rounded cursor-pointer px-24 py-5" href="../../Aproflow_Package-ETHDenver.pdf" download="Aproflow_Package-ETHDenver.pdf">
-            <h2 class="text-white font-bold text-center text-2xl italic">ETHDenver Special  Details</h2>
+      <div class="text-center mb-5 text-2xl">
+        <u><em>You supply <strong>content</strong> --- we provide <strong>everything else</strong></em></u>
+      </div>
+
+      <ul class="list-disc ml-20 text-lg pt-1">
+        <li><strong>#BUIDL Week Event Coordination</strong> (venue booking, audio/visual, refreshments)</li>
+        <li><strong>Pre-Event Marketing</strong> to students and local blockchain network of 2,750+ individuals</li>
+        <li><strong>Photography/Videography/Live Streaming</strong></li>
+        <li><strong>Standard Meetup Duration or Full-Day Bootcamp Events</strong></li>
+        <li><strong>Student Connections </strong>Prior, during and post-ETHDenver</li>
+
+      </ul>
+
+      <div class="flex justify-center object-center w-full pt-8">
+        <a class="block bg-blue-light hover:bg-blue-700 tracking-wide rounded cursor-pointer px-24 py-3" href="../../Aproflow_Package-ETHDenver.pdf" download="Aproflow_Package-ETHDenver.pdf">
+          <h2 class="text-white font-bold text-center text-2xl italic">Aproflow + ETHDenver <br>Service Details</h2>
         </a>
       </div>
 
     </div>
+    <div class="services container-inner mx-auto text-xl border-t border-gray-500 pt-5 pb-12">
+      <p class="text-xl">
+        <br> <strong><em class="text-blue-dark">Apro</em><em class="text-blue-light">flow</em></strong> is a fully-integrated educational program and network focused on emerging trends, curriculum enhancement, industry partnerships, and authentic
+        student experiences. We offer ongoing curriculum integration opportunities and programming throughout the year as part of our flow with multiple university partner institutions.
+      </p>
+    </div>
 
-    <div class="flex-12 font-bold w-full text-center pt-4 text-2xl italic text-blue-400 leading-tight pb-12">Blurring boundaries between earning, learning, and living</div>
-
+  </div>
   <div class="contact bg-background-secondary py-8 overflow-x-hidden" id="contact">
     <div class="flex justify-center object-center w-full">
       <a class="block bg-blue-400 hover:bg-blue-light tracking-wide rounded cursor-pointer px-20 py-3" href="mailto:hello@aproflow.com">
-        <h2 class="text-white font-bold text-center text-2xl">Contact Us</h2>
+        <h2 class="text-white font-bold text-center text-2xl">Contact Us To Discuss your Event</h2>
       </a>
     </div>
   </div> <!-- end contact -->
@@ -62,6 +73,12 @@
 
 </Layout>
 </template>
+
+<style scoped>
+li {
+  margin: 4px 0;
+}
+</style>
 
 <script>
 export default {
